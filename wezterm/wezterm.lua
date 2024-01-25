1467f82f9f41e50f613275e04dce70730a00fbe3
frain <F0AIN@users.noreply.github.com>
2024-01-25 21:55:22 +0900

    fix: font変更

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,15 +6,15 @@ if wezterm.config_builder then
   config = wezterm.config_builder();
 end
 
-config.color_scheme = "Dracula (Official)"
+config.color_scheme = 'Dracula (Official)'
 config.font_size = 14.0
 config.font = wezterm.font {
-  family = "JetBrains Mono",
-  harfbuzz_features = { "catl=0", "clig=0", "liga=0" }
+  family = 'PlemolJP Console NF',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 }
 
 config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 
 config.default_prog = { "/bin/zsh", "-l", "-c", "zellij" }
 
