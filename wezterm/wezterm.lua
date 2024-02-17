e68d1c1bf96c99d7ba7280c4741e25f010fa9081
frain <F0AIN@users.noreply.github.com>
2024-02-18 00:55:09 +0900

    chore: 色々調整

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,14 +8,15 @@ end
 
 config.color_scheme = 'Dracula (Official)'
 config.font_size = 14.0
-config.font = wezterm.font {
-  family = 'PlemolJP Console NF',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-}
+config.font = wezterm.font_with_fallback({
+  { family = 'Monaspace Xenon' },
+  { family = 'PlemolJP Console NF', harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }},
+  { family = 'PlemolJP Console NF', assume_emoji_presentation=true },
+})
 
 config.use_fancy_tab_bar = false
 config.window_decorations = 'RESIZE'
 
-config.default_prog = { "/bin/zsh", "-l", "-c", "zellij" }
+config.default_prog = { "/bin/zsh", "-l", "-c", "zellij attach -c 'work'" }
 
 return config
