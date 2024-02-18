5fa33a3541147c62c219988e7e12a06fc3d8dec4
frain <F0AIN@users.noreply.github.com>
2024-02-18 11:21:16 +0900

    chore: コメント追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 
 local config = {}
-
 if wezterm.config_builder then
   config = wezterm.config_builder();
 end
 
 config.color_scheme = 'Dracula (Official)'
+
+-- font
 config.font_size = 14.0
 config.font = wezterm.font_with_fallback({
   -- プログラミング用フォント
@@ -15,7 +16,8 @@ config.font = wezterm.font_with_fallback({
   { family = 'FirgeNerd Console', harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }},
 })
 
-config.use_fancy_tab_bar = false
+-- UI
+config.enable_tab_bar = false
 config.window_decorations = 'RESIZE'
 
 config.default_prog = { "/bin/zsh", "-l", "-c", "zellij attach -c 'work'" }
